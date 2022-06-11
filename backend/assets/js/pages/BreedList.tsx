1e927fa2f4d24c6bbca01444864d579087a90066
Jake Prem <jakeprem@gmail.com>
2022-06-11 17:54:39 -0400

    Implement BreedList component with data fetched from API

--- backend/assets/js/pages/BreedList.tsx
+++ backend/assets/js/pages/BreedList.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { Breed } from "../models/breed.model";
+import {getBreedList} from '../services/breed-service'
+
 function BreedList() {
+  const [breeds, setBreeds] = useState<Breed[]>([]);
+
+  useEffect(() => {
+      getBreedList().then(setBreeds)
+  }, [])
 
   return (
     <div>
@@ -11,7 +21,28 @@ function BreedList() {
           All your favorite dogs, now in one place.
         </p>
       </div>
+      <div className="mt-12 grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
+        {breeds.map(BreedCard)}
+      </div>
     </div>
+  )
+}
+
+function BreedCard(breed: Breed): JSX.Element {
+  return (
+    <article className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm" key={breed.id.toString()}>
+      <Link to={`/breeds/${breed.id.toString()}`} state={{ breedName: breed.name}}>
+        <img src={breed.image} alt={breed.name} loading="lazy" className="w-full h-48 rounded-t-md" />
+        <div className="pt-3 ml-4 mr-2 mb-3">
+          <h3 className="text-xl text-gray-900">
+            {breed.name}
+          </h3>
+          <p className="text-gray-400 text-sm-mt-1">
+            {breed.description}
+          </p>
+        </div>
+      </Link>
+    </article>
   )
 }
 
