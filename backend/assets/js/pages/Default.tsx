a44f63a09fa569a0887fe379c1d87bca31592a86
Jake Prem <jakeprem@gmail.com>
2022-06-12 00:08:47 -0400

    Update .gitignore to ignore uploaded files and update some static text

--- backend/assets/js/pages/Default.tsx
+++ backend/assets/js/pages/Default.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Default() {
   return (
-    <h1>Hello there!</h1>
+    <Link to="breeds" className="">
+      <button type="button" className="py-2.5 px-7 my-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">
+        View Dog Breeds
+      </button>
+    </Link>
   )
 }
 
