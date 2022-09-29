e766e8868ec897071a62bfdbc647cf34aded78cf
Nic Ficca <nicficca@gmail.com>
2022-09-29 15:31:34 -0400

    Add web to docker setup

--- bin/run.sh
+++ bin/run.sh
@@ -2,6 +2,15 @@
 
 set -ex
 
-mix ecto.create
-mix ecto.migrate
+if [ "$RESET_DATABASE" = "true" ]
+then
+  mix ecto.drop -f
+  mix ecto.create
+  mix ecto.migrate
+  mix run priv/repo/seeds.exs
+else
+  mix ecto.create
+  mix ecto.migrate
+fi
+
 mix phx.server
